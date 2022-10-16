911e6041497ec0dc2d173ffb93b7c3c8a775379d
Drinnn <pedro_lontro@hotmail.com>
2022-10-16 17:43:15 -0300

    chore: add eslint exception

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,5 @@ app.get('/', (req, res) => {
   return res.json({ message: 'Hello, World!' });
 });
 
+// eslint-disable-next-line no-console
 app.listen(3333, () => console.log('Server is running!'));

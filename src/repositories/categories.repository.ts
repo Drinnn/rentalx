2842ac048bbbdda64e73f5db8e22afbf1ccb1e65
Drinnn <pedro_lontro@hotmail.com>
2022-10-16 18:18:38 -0300

    feat: add getAll categories route

--- src/repositories/categories.repository.ts
+++ src/repositories/categories.repository.ts
@@ -23,6 +23,10 @@ export class CategoriesRepository {
 
     this.categories.push(category);
   }
+
+  list(): Category[] {
+    return this.categories;
+  }
 }
 
 export default CategoriesRepository;

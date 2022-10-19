dd54c315c88fb06ecbd60363a406ff77d4bc0da7
Drinnn <pedro_lontro@hotmail.com>
2022-10-19 13:32:46 -0300

    chore: create categories table migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,3 @@
-import { DataSource } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-const appDataSource = new DataSource({
-  type: 'postgres',
-  host: 'db',
-  port: 5432,
-  username: 'rentalx',
-  password: 'rentalx',
-  database: 'rentalx',
-});
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
-  })
-  .catch(err => {
-    console.error('Error during Data Source initialization', err);
-  });
+createConnection();

c93bfe19e95f05830f7c2542bd5eee0e24696726
Drinnn <pedro_lontro@hotmail.com>
2022-10-20 13:27:29 -0300

    refactor: make specifications to use new injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,14 @@
 import { container } from 'tsyringe';
 import ICategoriesRepository from '../../modules/cars/repositories/categories-repository.interface';
 import CategoriesRepository from '../../modules/cars/repositories/implementations/categories.repository';
+import SpecificationsRepository from '../../modules/cars/repositories/implementations/specifications.repository';
+import ISpecificationsRepository from '../../modules/cars/repositories/specifications-repository.interface';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
   CategoriesRepository,
+);
+container.registerSingleton<ISpecificationsRepository>(
+  'SpecificationsRepository',
+  SpecificationsRepository,
 );

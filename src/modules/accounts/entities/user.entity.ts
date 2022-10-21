9431acf85b09e2e3f9b3b785176c946a168ad883
Drinnn <pedro_lontro@hotmail.com>
2022-10-20 23:01:54 -0300

    chore: add user migration and entity

---
+++ src/modules/accounts/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: 'driver_licence' })
+  driverLicence: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export default User;

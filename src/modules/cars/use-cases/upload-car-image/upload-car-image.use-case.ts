8918d93890b8457acfc7612d917314a30daa06c3
Drinnn <pedro_lontro@hotmail.com>
2022-11-15 23:35:34 -0300

    refactor: add checking for existent car

--- src/modules/cars/use-cases/upload-car-image/upload-car-image.use-case.ts
+++ src/modules/cars/use-cases/upload-car-image/upload-car-image.use-case.ts
@@ -1,4 +1,6 @@
 import ICarsImagesRepository from '@modules/cars/repositories/cars-images-repository.interface';
+import ICarsRepository from '@modules/cars/repositories/cars-repository.interface';
+import AppError from '@shared/errors/app.error';
 import { inject, injectable } from 'tsyringe';
 
 export interface IUploadCarImageUseCaseInputDto {
@@ -9,12 +11,19 @@ export interface IUploadCarImageUseCaseInputDto {
 @injectable()
 export class UploadCarImageUseCase {
   constructor(
+    @inject('CarsRepository')
+    private readonly carsRepository: ICarsRepository,
     @inject('CarsImagesRepository')
     private readonly carsImagesRepository: ICarsImagesRepository,
   ) {}
 
   async execute(dto: IUploadCarImageUseCaseInputDto): Promise<void> {
     const { carId, imageNames } = dto;
+
+    const car = await this.carsRepository.findById(carId);
+    if (!car) {
+      throw new AppError('Car doesn´t exists.');
+    }
 
     await Promise.all(
       imageNames.map(async imageName => {
